15c9e308120c364000eb42c6c3f305a6dce4f30c
Brandon Michael Taft <btaft6278@gmail.com>
2021-11-30 12:10:46 -0500

    Add files via upload

---
+++ twilio_sms.php
@@ -0,0 +1,20 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Twilio\Rest\Client;
+
+function send_sms($name, $email)
+{
+    $sid = "";
+    $token = ""; 
+
+    $client = new Client($sid, $token);
+    $client->messages->create(
+        '7706346786', 
+        [
+            'from' => '+12676929007', 
+            'body' => 'Congratulations! YOU FINISHED THE LIST! '
+        ]
+    );
+}
